cc941517163b6cc21b5fbf7269121914573b755f
jzanetii4 <hangqiy@163.com>
2019-11-01 20:00:29 +1100

    i :w

--- src/reducer/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from "redux";
-import {postReducer} from './postReducer'
-import {createTaskReducer} from './tasks.redux'
-import auth from "./auth";
-
-export default combineReducers({postReducer,createTaskReducer,auth})
